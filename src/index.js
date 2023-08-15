f3b7ba0aa975b1144e2574e1877106422dd1e739
Anita Guerini <anita.guerini@avanade.com>
2023-08-15 13:21:52 +0200

    update to react 18

--- src/index.js
+++ src/index.js
@@ -1,19 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import reportWebVitals from "./reportWebVitals";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./index.css";
+import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "router";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const container = document.getElementById("root");
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<RouterProvider router={router} />);
